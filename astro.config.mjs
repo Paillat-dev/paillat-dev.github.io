fee8d0597fadf25ee20ae7cbb841befcf8258775
Paillat-dev <me@paillat.dev>
2026-02-11 15:47:59 +0100

    :wrench: Add site URL to Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
-
+  site: "https://paillat.dev",
   vite: {
     plugins: [tailwindcss()],
   },
